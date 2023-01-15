f2df543cd5687b362ebf7fffabc1b8ece959e480
Nadia <nadya-koshelkovskaya@yandex.ru>
2023-01-15 17:59:22 +0300

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String args[]) {
-        int x=200; // начальная сумма на счету
-        int y=1400; //  сумма пополнения счёта
-        if (y>=1000) {
-            int i=y/100; // количество начисляемых бонусных рублей при пополнении на 1000+ руб.
-            System.out.println("Сумма бонусных рублей - " + i);
-            int z=x+y+i; // итоговый счёт
-            System.out.println("Итоговый счёт - " + z + " рублей");
-        }
-        if (y<1000) {
-            int i = 0; // количество начисляемых бонусных рублей при пополнении меньше 1000 руб.
-            System.out.println("Сумма бонусных рублей - " + i);
-            int z=x+y+i; // итоговый счёт
-            System.out.println("Итоговый счёт - " + z + " рублей");
-        }}
+        int currentBalance = 200;
+        int plusSum = 999; //  сумма пополнения счёта
 
+        int bonus = 0;
+        if (plusSum > 1000) {
+            bonus = plusSum / 100; // количество начисляемых бонусных рублей при пополнении на 1000+ руб.
+        }
+        int newBalance = currentBalance + plusSum + bonus; // итоговый счёт
+
+        System.out.println("Сумма бонусных рублей - " + bonus);
+        System.out.println("Итоговый счёт - " + newBalance + " рублей");
     }
+
+}
